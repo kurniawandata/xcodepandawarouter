b860b99cea47e04f119ae9b416d35f01db88a9c5
Kurniawan <38490299+kurniawandata@users.noreply.github.com>
2018-05-11 01:02:09 +0700

    Update xcoderouter.sh

--- xcoderouter.sh
+++ xcoderouter.sh
@@ -7,7 +7,7 @@ clear
 echo "=====================================================================";
 echo " X-code Pandawa Router for Ubuntu 18.04 Server                       ";
 echo " Progammer : Kurniawan. xcode.or.id                                  ";
-echo " Version 1.0 Beta 3                                                  ";
+echo " Version 1.0 Beta 3 (11/05/2018)                                     ";
 echo "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=";
 echo " Router & Server pendukung router                                    ";
 echo " [1]  Install X-code Pandawa (Untuk ganti nama ke eth0 dan eth1)     ";
